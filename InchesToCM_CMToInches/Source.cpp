69e864d89d5e7abcf4ba5cfe6404e34bda68ef34
damindra <damindra@damindra.co.uk>
2024-09-08 13:51:19 +0100

    Inches to CM and CM to Inches Program

---
+++ InchesToCM_CMToInches/Source.cpp
@@ -0,0 +1,52 @@
+// Program that converts Inches to Centimeters AND Centimeters to Inches
+
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+int main()
+{
+	constexpr double cm_per_inch = 2.54; // number of centimeters in an inch
+	double length = 1; // length in inches or centimeters
+
+	char unit = ' '; // a space is not a unit
+
+	cout << "This program converts inches to centimeters or centimeters to inches.\n";
+	cout << "Please use C for centimeters or I for inches.\n";
+	cout << "Please enter a length ";
+
+	cin >> length ;
+
+	cout << "Please enter a unit (C or I): ";
+
+	cin >> unit;
+	unit = toupper(unit); // make the unit uppercase
+
+	switch (unit)
+	{
+	case 'C':
+		cout << length << " cm == " << length / cm_per_inch << " in\n";
+		break;
+	case 'I':
+		cout << length << " in == " << cm_per_inch * length << " cm\n";
+		break;
+	default:
+		cout << "Sorry, I don't know a unit called '" << unit << "'\n";
+		break;
+	}
+
+	
+	//if (unit == 'C') // if the unit is centimeters
+	//{
+	//	cout << length << " cm == " << length / cm_per_inch << " in\n";
+	//}
+	//else if (unit == 'I' ) // if the unit is inches
+	//{
+	//	cout << length << " in == " << cm_per_inch * length  << " cm\n";
+	//}
+	//else
+	//{
+	//	cout << "Sorry, I don't know a unit called '" << unit << "'\n";
+	//}
+}
