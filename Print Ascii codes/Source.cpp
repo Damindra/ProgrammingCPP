271ddaabbb3ee52aef218121684e2edd9f0d8382
damindra <damindra@damindra.co.uk>
2024-09-08 14:11:40 +0100

    Ascii character codes for a-z and A-Z

---
+++ Print Ascii codes/Source.cpp
@@ -0,0 +1,28 @@
+// This program prints the ascii value of characters a-z and A-Z
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char lowercaseLetters;
+	char uppercaseLetters;
+
+	cout << "ASCII values of lowercase and uppercase letters are as follows: " << endl;
+	cout << "--------------------------------------------------------------" << endl;
+	for (lowercaseLetters = 'a'; lowercaseLetters <= 'z'; lowercaseLetters++)
+	{
+		cout << lowercaseLetters << " = " << int(lowercaseLetters) << endl;
+	}
+	for (uppercaseLetters = 'A'; uppercaseLetters <= 'Z'; uppercaseLetters++)
+	{
+		cout << uppercaseLetters << " = " << int(uppercaseLetters) << endl;
+	}
+	return 0;
+}
+
+
+
+
+
