d8ab27b8f0517e8f9f43f5c5745b1a71a80828f1
damindra <damindra@damindra.co.uk>
2024-09-08 13:15:31 +0100

    Hello World

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,9 @@
+// HelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "../std_lib_facilities.h"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
