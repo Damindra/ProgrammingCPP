e8ae870610b44530e4bd10ae7dcfca63957e1de2
damindra <damindra@damindra.co.uk>
2024-09-08 15:47:43 +0100

    added more comments to understand code

---
+++ SimpleDictionary/Source.cpp
@@ -0,0 +1,53 @@
+// Simple dictionary program using vector
+
+
+#include <iostream>
+
+#include <vector>
+
+#include <string>
+
+#include <algorithm>
+
+using namespace std;
+
+int main()
+
+{
+
+	vector<string> words;
+
+	string temp;
+
+	cout << "Enter words separated by space. Enter 'finishedlist' when finished." << endl;
+
+	while (cin >> temp)
+
+	{
+
+		if (temp == "finishedlist")
+
+			break;
+
+		words.push_back(temp); // add word to vector
+
+	}
+
+	sort(words.begin(), words.end()); // sort the words alphabetically
+
+	cout << "Words in sorted order:" << endl;
+
+	for (int i = 0; i < words.size(); i++) {
+
+		if (i == 0 || words[i] != words[i - 1]) { // check for duplicates since we sorted the words
+			cout << words[i] << endl;
+		}
+		else
+		{
+			cout << "Duplicate word: " << words[i] << endl;
+		}
+	}
+	return 0;
+
+
+}
