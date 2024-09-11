a3ff884e5d298229b5170fb84085aeb84ffb59a8
damindra <damindra@damindra.co.uk>
2024-09-11 19:51:55 +0100

    Stroustrup Programming Principles and Practice C++ Sample Code

--- CalcV23/CalcV23.cpp
+++ CalcV23/CalcV23.cpp
@@ -5,20 +5,43 @@ import std;
 
 using namespace std;
 
-
+// error() simply disguises throws:
+void error(string s)
+{
+	throw runtime_error(s);
+}
 
 int main()
 {
-	cout << "Please enter expression (we can handle + and -):";
+	cout << "Please enter expression (we can handle + , -, * and /)\n";
+	cout << "Add an x to end expression (e.g., 1+2*3x): ";
 	int lval = 0;	// left value
 	int rval = 0;	// right value
-	char op = 0;	// operator
-	int res = 0;	// result
-	cin >> lval >> op >> rval; // read something like 1 + 3
+	cin >> lval;	// read left most operand
+	if (!cin)
+		error("no first operand");
 
-	if (op == '+')
-		res = lval + rval; // addition
-	else if (op == '-')
-		res = lval - rval; // subtraction
-	cout << "Result: " << res << '\n';
+	for (char op; cin >> op;) { // read operator and right hand repeatedly
+		if (op != 'x') cin >> rval; // read right most operand if not equal to x
+		if (!cin) error("no second operand"); // check for right operand, so not a '+ - * /' operator
+
+		switch (op) {
+		case '+':
+			lval += rval; // add: lval = lval + rval
+			break;
+		case '-':
+			lval -= rval; // subtract: lval = lval - rval
+			break;
+		case '*':
+			lval *= rval; // multiply: lval = lval * rval
+			break;
+		case '/':
+			lval /= rval; // divide: lval = lval / rval
+			break;
+		default: // if not a '+ - * /' operator print result
+			cout << "Result: " << lval << '\n';
+			return 0;
+		}
+	}
+	error("bad expression");
 }
